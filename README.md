6d9b64acd8891f66970507b006ea53e25f48bebf
elliotkoss <elliotkoss@gmail.com>
2021-10-17 18:20:10 -0500

    v0.0.1 - Node + Express + Auth0 + Axios

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# node-api
-Node API Template
+# Node API Template
+
+### Latest Version
+
+v0.0.1 - Oct 17, 2021
+
+### Notes
+
+Hello World! I built a Node JS API using Express for routes, Auth0 for security, and Axios to execute a third-party API call. I watched this [great Node tutorial](https://www.youtube.com/watch?v=pKd0Rpw7O48), read the Auth0 docs, and then used Postman's sample Node code plus some Googling to determine Axios was the right library for API calls.
+
+### Clean-up
+
+1. Run this set-up from a clean machine to identify any updates to the ReadMe since there may need to be some other Node install required.
+
+2. Remove any unnecessary Node packages.
+
+# Getting Started
+
+This is a Node JS API using Express for routes, Auth0 for security, and Axios to execute a third-party API call.
+
+In general, the naming should be intuitive, where you would say, 'get this endpoint data' or 'post data to this endpoint'.
+
+Feel free to use this as a starting point when building your own API.
+
+1. Run `nodemon index.js`.
+
+2. Use Postman to check each of the API URLs. The responses should be intuitive:
+
+    a.  GET http://localhost:3000/
+    b.  GET http://localhost:3000/api/courses
+    c.  POST http://localhost:3000/api/courses
+            raw -> JSON input for the post
+            {
+                "name": "coursea 1"
+            }
+
+            TIP: Rerun 2.b to confirm value was added
+    d.  GET http://localhost:3000/api/courses/1
+    e.  PUT http://localhost:3000/api/courses/1
+            raw -> JSON input for the post
+            {
+                "name": "coursea 1"
+            }
+
+            TIP: Rerun 2.b to confirm value was updated
+    f.  DELETE http://localhost:3000/api/courses/1
+            TIP: Rerun 2.b to confirm value was deleted
+    g.  GET http://localhost:3000/api/hipster-ipsum
+            This is an example of a third-party API fetch and response.
+
+3. To add Auth0, which enforces security credentials, you'll need to follow some tutorials first. If this is your first time, go ahead and do some reading since security is important and understanding what you're doing will save hours of frustration later.
+
+    a. Tutorials
+        i.      https://auth0.com/docs/quickstart/backend/nodejs/01-authorization
+        ii.     https://auth0.com/docs/quickstart/backend/nodejs/02-using
+        iii.    https://auth0.com/docs/get-started/create-apps/machine-to-machine-apps
+    b. Validate set-up
+        GET http://localhost:3000/secure
+            header->Authorization (Bearer token as noted in the tutorial 3.a.ii above)
+
+# Contact Me
+
+Feel free to reach out on Twitter, @elliotkoss. DMs open.
+
+# Donations
+
+I intend to always keep this freely available, but if you'd like to send a donation, I'd be much appreciative.
+
+ETH: elliotkoss.eth (ETH only set-up for this ENS - adding more wallets later)
+Venmo: @Elliot-Koss
+Cash App: $elliotkoss1
---
+++ index.js
@@ -0,0 +1,126 @@
+const Joi = require('joi');
+const express = require('express');
+const { request } = require('express');
+const app = express();
+var axios = require('axios');
+const crypto = require("crypto");
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+
+app.use(express.json());
+
+// Authorization middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and 
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://YOUR_DOMAIN/.well-known/jwks.json` //<-----------ADD YOUR OWN---------------
+  }),
+
+  // Validate the audience and the issuer.
+  audience: 'YOUR_API_IDENTIFIER', //<-----------ADD YOUR OWN---------------
+  issuer: [`https://YOUR_DOMAIN/`], //<-----------ADD YOUR OWN---------------
+  algorithms: ['RS256']
+});
+
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
+];
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.get('/secure', checkJwt, (req, res) => {
+  res.send('This is a secure connection');
+});
+
+app.get('/api/courses', (req, res) => {
+  res.send(courses);
+});
+
+app.post('/api/courses', (req, res) => {  
+  const { error } = validateCourse(req.body);
+  if ( error ) return res.status(400).send(error.details[0].message);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course with the given ID was not found');
+
+  res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course with the given ID was not found');    
+
+  const { error } = validateCourse(req.body);
+  if ( error ) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course with the given ID was not found');
+
+  //Delete
+  const index = courses.indexOf(course);
+  console.log(index);
+  courses.splice(index, 1);
+
+  //return the same course
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  });
+
+  return schema.validate(course);
+}
+
+app.get('/api/hipster-ipsum', (req, res) => {
+  
+  var config = {
+    method: 'get',
+    url: 'http://hipsum.co/api/?type=hipster-centric&sentences=3',
+    headers: { }
+  };
+
+  axios(config)
+  .then(function (response) {    
+    body = JSON.stringify(response.data);    
+    
+    res.send(body);
+  })
+  .catch(function (error) {
+    console.log(error);
+    res.send(error);
+  });
+  
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
+
+
